ae7ee57bc8769e222d366f502283a5540f0fc06f
Vito Gamberini <vito@gamberini.email>
2023-08-09 06:04:48 -0400

    fix(nyu-util): Use config fixup

--- ports/nyu-util/portfile.cmake
+++ ports/nyu-util/portfile.cmake
@@ -6,10 +6,9 @@ vcpkg_from_github(
   HEAD_REF main
 )
 
-vcpkg_cmake_configure(
-  SOURCE_PATH ${SOURCE_PATH}
-)
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
