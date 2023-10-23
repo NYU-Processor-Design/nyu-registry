a36d116b3729becdd2e85362ad3480bb0bfa70ca
Vito Gamberini <vito@gamberini.email>
2023-10-23 02:14:46 -0400

    chore(nyu-amba): update

--- ports/nyu-amba/portfile.cmake
+++ ports/nyu-amba/portfile.cmake
@@ -1,19 +1,23 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO NYU-Processor-Design/nyu-amba
-  REF 760b94fc8dbcf996f82a280380425c2d8d977387
-  SHA512 9df11408425df17279f1001958abace2ce59fd2a8a82f341550135cc13d6e1d6bce4a9cdc78625cdec120c2ecdb5f8ee430f1168da0ef9eb3bdb71f0486217af
+  REF 96e4151f4a674aea9540fc3b4273e2716baa3e7e
+  SHA512 dacf20e80fd668f23328d2e87ccb155beb7be040191c6ca9aa36fef785488efcdda55220eed5701358e4ffda63024a395ad667a8886482c808821fd3a97bbcaa
   HEAD_REF main
 )
 
 vcpkg_cmake_configure(
   SOURCE_PATH ${SOURCE_PATH}
   OPTIONS
-    -DNYU_BUILD_TESTS=FALSE
+  -DNYU_BUILD_TESTS=FALSE
 )
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+
 vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/License)
+
 file(
   INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage
   DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
